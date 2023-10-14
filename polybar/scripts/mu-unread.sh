6de79aefbd0c8dbe97c44909f821dfea06dbc951
David Adams <dbadams@iinet.net.au>
2023-10-14 16:12:52 +0800

    wacom and small ssh updates

--- polybar/scripts/mu-unread.sh
+++ polybar/scripts/mu-unread.sh
@@ -3,7 +3,14 @@
 # sync maildir
 # mbsync -a > /dev/null 2>&1
 
-# get unread count
-COUNT=$(mu find flag:unread and flag:new | wc -l)
+# Get unread count and capture the output along with any error messages
+COUNT=$(mu find flag:unread and flag:new 2>&1)
 
-echo $COUNT;
+# Check if there was an error message
+if [[ $COUNT == "error: no matches for search expression" ]]; then
+    echo "∅" # Return a smiley face
+else
+    # Count the number of lines in the output
+    COUNT=$(echo "$COUNT" | wc -l)
+    echo $COUNT
+fi
